d66e8b827b9ca58b7922720ead97838052a0874a
Nils Backe <nils.backe@appfolio.com>
2020-05-15 12:40:57 -0700

    Updates

---
+++ precache-manifest.93acfd3b0e22d44a2272ecd10bc091d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157d48198014db248de3d7f9b00e9876",
+    "url": "/bluefin-police-app-demo/index.html"
+  },
+  {
+    "revision": "afc8c3c897a7c618be6c",
+    "url": "/bluefin-police-app-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b287ebc466944566014f",
+    "url": "/bluefin-police-app-demo/static/js/2.479bb5cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bluefin-police-app-demo/static/js/2.479bb5cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc8c3c897a7c618be6c",
+    "url": "/bluefin-police-app-demo/static/js/main.79de0f90.chunk.js"
+  },
+  {
+    "revision": "995dacd27a44ab1c9c1d",
+    "url": "/bluefin-police-app-demo/static/js/runtime-main.13cabd7a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6beb0ad6ad392656df2afe9466423c3a.js"
+  "/bluefin-police-app-demo/precache-manifest.93acfd3b0e22d44a2272ecd10bc091d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bluefin-police-app-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
